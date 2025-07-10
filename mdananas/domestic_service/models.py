f893a610aff616b180b5d676d1a7274c7e73637b
Sergeev <SergeevM@herorus.ru>
2025-07-10 19:17:50 +0300

    implant acc files upload done

--- mdananas/domestic_service/models.py
+++ mdananas/domestic_service/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from root_service.models import Cu, Mix
 
 class DM_TMP_BY_Sale(models.Model):
     date_year = models.CharField(max_length=255)
@@ -21,6 +22,7 @@ class DM_TMP_RU_Sale(models.Model):
     store_dm = models.CharField(max_length=255)
     store_name = models.CharField(max_length=255)
     material = models.CharField(max_length=255)
+    purchase = models.CharField(max_length=255)
     volume = models.CharField(max_length=255)
     purchase_price_value = models.CharField(max_length=255)
     fact_price_value = models.CharField(max_length=255)
@@ -28,3 +30,13 @@ class DM_TMP_RU_Sale(models.Model):
     class Meta:
        managed = False
        db_table = '[20_DM].[DM_TMP_RU_Sales]'
+
+class DM_PIVOT_SKU(models.Model):
+    root_cu = models.ForeignKey(Cu, on_delete=models.PROTECT, null=True, )
+    root_mix = models.ForeignKey(Mix, on_delete=models.PROTECT, null=True)
+    material = models.TextField()
+    is_last_upload = models.BooleanField()
+    is_mix = models.BooleanField()
+    class Meta:
+       managed = False
+       db_table = '[20_DM].[DM_PIVOT_SKU]'
--- mdananas/domestic_service/views.py
+++ mdananas/domestic_service/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse, JsonResponse
+from django.http import HttpResponse, JsonResponse, HttpResponseRedirect
 from django.urls import reverse
 from django.core.cache import cache
 from .forms import *
@@ -11,6 +11,7 @@ from xml.etree import ElementTree as ET
 from pyxlsb import open_workbook, convert_date
 import tempfile
 import csv
+import os
 
 def main_page(request):
     return HttpResponse("Hello, world!")
@@ -38,6 +39,7 @@ def process_by_file(file, request):
         ))
         cache.set(f'by_file_progress_{request.session.session_key}', (index + 1) / total_rows * 100, 300) 
     cache.set(f'by_file_active_{request.session.session_key}', 0, 300)
+    cache.set(f'by_file_progress_{request.session.session_key}', 0, 300) 
     return result
 
 def process_decade_file(file, request):
@@ -70,6 +72,8 @@ def process_implant_file(file, request):
                             object.store_name = cell.v
                         case key if key == header.get('КодТовара'):
                             object.material = cell.v
+                        case key if key == header.get('Приходы от поставщика ШТ'):
+                            object.purchase = cell.v
                         case key if key == header.get('Реализация для оплат ШТ'):
                             object.volume = cell.v
                         case key if key == header.get('Товарооборот ЗЦ'):
@@ -83,6 +87,7 @@ def process_implant_file(file, request):
                 result.append(object)
                 cache.set(f'implant_file_progress_{request.session.session_key}', int(current_row / total_rows * 100), 300)
     cache.set(f'implant_file_active_{request.session.session_key}', 0, 300)
+    cache.set(f'implant_file_progress_{request.session.session_key}', 0, 300)
     return result
 
 
@@ -92,12 +97,14 @@ def bulk_insert_to_sqlserver(csv_path, model):
             BULK INSERT {model._meta.db_table}
             FROM '{csv_path}'
             WITH (
-                FORMAT = 'CSV',
+                CODEPAGE = '65001',
                 FIRSTROW = 2,
-                FIELDTERMINATOR = ',',
-                ROWTERMINATOR = '\n'
+                ROWTERMINATOR = '\r\n',
+                FIELDTERMINATOR = ';'
             )
         """)
+    if os.path.exists(csv_path):
+        os.remove(csv_path)
 
 def upload_file(file_data, model, proc, big=False):
     with connections['ideal'].cursor() as cursor:
@@ -105,14 +112,14 @@ def upload_file(file_data, model, proc, big=False):
         with IdealSchemaEditor(connection=connections['ideal']) as schema_editor:
             schema_editor.create_model(model)
             if big:
-                with tempfile.NamedTemporaryFile(mode='w+', suffix='.csv', delete=True) as tmp_file:
-                    writer = csv.writer(tmp_file)
+                NETWORK_FOLDER = r"\\hru03\Public\BI\40. ERP data for SQL"
+                with tempfile.NamedTemporaryFile(mode='w+', suffix='.csv', encoding='utf-8', dir=NETWORK_FOLDER, delete=False) as tmp_file:
+                    writer = csv.writer(tmp_file, delimiter=';', lineterminator='\n')
                     writer.writerow([f.name for f in model._meta.fields])  # Заголовки
-                    for obj in model.objects.all():
+                    for obj in file_data:
                         writer.writerow([getattr(obj, f.name) for f in model._meta.fields])
-                    # Пример передачи файла в BULK INSERT (через pyodbc)
-                    tmp_file.seek(0)  # Перемещаем указатель в начало
-                    bulk_insert_to_sqlserver(tmp_file.name, model)  # Ваша функция загрузки
+                    tmp_file.flush()
+                bulk_insert_to_sqlserver(tmp_file.name.replace(os.sep, '/'), model)
             else:
                 model.objects.bulk_create(file_data)
             cursor.execute(f"EXEC {proc}")
@@ -127,6 +134,14 @@ def get_progress(request):
 def dm_page(request):
     cache.set(f'by_file_active_{request.session.session_key}', 1, 300)
     cache.set(f'implant_file_active_{request.session.session_key}', 1, 300)
+    if 'dm_pivot_sku_suo' in request.GET:
+        response = HttpResponseRedirect(request.path)
+        response.set_cookie('dm_pivot_sku_suo', 'on' if request.GET.get('dm_pivot_sku_suo') == 'on' else 'off', max_age = 86400)
+        return response
+    dm_pivot_sku_suo = request.COOKIES.get('dm_pivot_sku_suo', 'on') == 'on'
+    existing_pivot_sku = DM_PIVOT_SKU.objects.filter(is_last_upload=True) if dm_pivot_sku_suo else DM_PIVOT_SKU.objects.all()
+    existing_cu = Cu.objects.all().order_by('xcode_cu')
+    existing_mix = Mix.objects.all().order_by('xcode_mix')
     if request.method == 'POST':
         form = FileUploadForm(request.POST, request.FILES)
         if form.is_valid():
@@ -135,15 +150,29 @@ def dm_page(request):
                     upload_file(process_by_file(form.cleaned_data['file'], request), DM_TMP_BY_Sale, '[20_DM].[DM_PROC_MERGE_BY_Sales]')
                     return JsonResponse({'status': 'success','redirect_url': reverse('dm_page')})
                 case 'implant':
-                    #process_implant_file(form.cleaned_data['file'], request)
                     upload_file(process_implant_file(form.cleaned_data['file'], request), DM_TMP_RU_Sale, '[20_DM].[DM_PROC_MERGE_RU_Sales]', big=True)
                     return JsonResponse({'status': 'success','redirect_url': reverse('dm_page')})
                 case 'decade':
                     #upload_file(process_decade_file(form.cleaned_data['file'], request), DM_TMP_BY_Sale, '[20_DM].[DM_PROC_MERGE_BY_Sales]')
-                    process_decade_file(form.cleaned_data['file'], request)
-                    return JsonResponse({'status': 'success','redirect_url': reverse('dm_page')})
+                    #process_decade_file(form.cleaned_data['file'], request)
+                    #return JsonResponse({'status': 'success','redirect_url': reverse('dm_page')})
+                    pass
                 case _:
                     pass
+        else:
+            form = PivotSKUForm(request.POST)
+            if form.is_valid():
+                object = DM_PIVOT_SKU.objects.get(id = form.cleaned_data['pivot_sku_id'])
+                if form.cleaned_data['is_mix']:
+                    object.is_mix = True
+                    object.root_cu = None
+                    object.root_mix = Mix.objects.get(id = form.cleaned_data['root_mix']) if form.cleaned_data['root_mix'] else None
+                else:
+                    object.is_mix = False
+                    object.root_mix = None
+                    object.root_cu = Cu.objects.get(id = form.cleaned_data['root_cu']) if form.cleaned_data['root_cu'] else None
+                object.save()
     else:
         print(0)
-    return render(request, "domestic_service/dm_page.html", context={})
+    return render(request, "domestic_service/dm_page.html", context={'existing_pivot_sku': existing_pivot_sku, 'existing_cu': existing_cu, 'existing_mix': existing_mix,
+                                                                      'dm_pivot_sku_suo': dm_pivot_sku_suo})
