50dc0b1909656464e799570d9777950c3117a62d
Sergeev <SergeevM@herorus.ru>
2025-08-28 11:27:47 +0300

    bababebbeb

--- mdananas/datapull_service/urls.py
+++ mdananas/datapull_service/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import priceva_views
+from .views import priceva_views, root_views
 
 urlpatterns = [
     path('', priceva_views.main_page, name='main_page'),
+    path('root_upl/', root_views.main_page, name='root_main_page'),
 ]
---
+++ mdananas/datapull_service/views/root_views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render, HttpResponse
+from django.http import JsonResponse
+import requests
+import pandas as pd
+
+def main_page(request):
+    return HttpResponse("babababebebe")
